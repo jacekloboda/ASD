abdfa7ff77492a42f5568f32a7c8680dca923fe7
jacekloboda <jacekloboda55@gmail.com>
2025-04-13 13:26:45 +0200

    add offline03.py

--- offlines/offline03.py
+++ offlines/offline03.py
@@ -1,31 +1,23 @@
-# Dany jest graf nieskierowany G = (V, E) oraz dwa wierzchołki s, t ∈ V . Proszę zaproponować i
-# zaimplementować algorytm, który sprawdza, czy istnieje taka krawędź {p, q} ∈ E, której usunięcie
-# z E spowoduje wydłużenie najkrótszej ścieżki między s a t (usuwamy tylko jedną krawędź). Algo-
-# rytm powinien być jak najszybszy i używać jak najmniej pamięci. Proszę skrótowo uzasadnić jego
-# poprawność i oszacować złożoność obliczeniową
-
-# Algorytm należy zaimplementować jako funkcję która przyjmuje graf G oraz numery wierzchołków
-# s, t i zwraca dowolną krawędź spełniającą warunki zadania, lub None jeśli takiej krawędzi w G nie ma.
-# Graf przekazywany jest jako lista list sąsiadów, t.j. G[i] to lista sąsiadów wierzchołka o numerze i.
-# Wierzchołki numerowane są od 0. Funkcja powinna zwrócić krotkę zawierającą numery dwóch wierzchołków
-# pomiędzy którymi jest krawędź spełniająca warunki zadania, lub None jeśli takiej krawędzi nie ma.
-# Jeśli w grafie oryginalnie nie było ścieżki z s do t to funkcja powinna zwrócić None.
-
-
-from collections import deque
-
+# algorytm zwraca krawedz w grafie ktorej usuniecie wydluzy najkrotsza sciezke z s do t (usuniecia jedynego polaczenia to tez wydluzenie)
+#
+# dzialanie: uzywajac algorytmu bfs sprawdzam ktore krawedzie w G naleza do najkrotszych sciezek, za pomoca dfs szukam mostu w grafie zlozonym z najkrotszych sciezek i go zwracam
+#
+# zlozonosc czasowa: O(E+V)
 
 def bfs(G, s):
 
+    from collections import deque
+
+    n = len(G)
     q = deque()
-    V = [False for _ in range(len(G))]
-    D = [float('inf') for _ in range(len(G))]  # distance from s
+    V = [False] * n  # visited status
+    D = [float('inf')] * n  # distance from s
 
     q.append(s)
-    D[s] = 0
     V[s] = True
+    D[s] = 0
 
-    while len(q) > 0:
+    while q:  # q not empty
 
         u = q.popleft()
 
@@ -34,42 +26,82 @@ def bfs(G, s):
             if not V[v]:
 
                 V[v] = True
-                q.append(v)
                 D[v] = D[u]+1
+                q.append(v)
 
     return D
+
+
+def find_bridge(G, in_Paths):
+
+    n = len(G)
+    V = [False] * n  # visited status
+    T = [0] * n  # discovery time
+    low = [0] * n  # for indentifying bridges
+    Bridges = []
+    time = 1
+
+    def dfs_visit(u, parent):
+
+        nonlocal time
+
+        V[u] = True
+        T[u] = time
+        low[u] = time
+        time += 1
+
+        for v in G[u]:
+
+            if not in_Paths[v]:
+                continue
+
+            if not V[v]:
+
+                dfs_visit(v, u)
+
+                if low[v] > T[u]:
+
+                    Bridges.append((u, v))
+
+                low[u] = min(low[u], low[v])
+
+            elif v != parent:
+
+                low[u] = min(low[u], T[v])
+
+    for u in range(n):
+
+        if not in_Paths[u]:
+            continue
+
+        if not V[u]:
+
+            dfs_visit(u, -1)
+
+    return Bridges[0] if len(Bridges) > 0 else None
 
 
 def longer(G, s, t):
 
     n = len(G)
 
-    dist_s = bfs(G, s)
-    dist_t = bfs(G, t)
+    Dist_s = bfs(G, s)
+    Dist_t = bfs(G, t)
 
-    shortest = dist_s[t]  # length of fastest route from s to t
+    if Dist_t[s] == float('inf'):
+
+        return None
+
+    in_Paths = [False] * n
 
     for u in range(n):
 
-        for v in G[u]:
+        if Dist_s[u] + Dist_t[u] == Dist_t[s]:
+            in_Paths[u] = True
 
-            if v <= u:
-                continue
+    for u in range(n):
 
-            if (dist_s[u] + dist_t[v] + 1 == shortest) or (dist_s[v] + dist_t[u] + 1 == shortest):
+        if not in_Paths[u]:
+            continue
 
-                if (dist_s[u] != dist_s[v]) and (dist_t[u] != dist_t[v]):
-
-                    return (u, v)
-
-    return None
-
-
-G = [[1, 2],
-     [0, 2],
-     [0, 1]]
-
-s = 0
-t = 2
-
-print(longer(G, s, t))
+    return find_bridge(G, in_Paths)
